78646f1cd4ab91f58497c59da3f1190fb195705f
Isuru Fernando <isuruf@gmail.com>
2019-08-20 00:32:18 -0500

    Use PolyMatrix instead of RawMatrix

--- sympy/matrices/tests/test_normalforms.py
+++ sympy/matrices/tests/test_normalforms.py
@@ -1,13 +1,12 @@
 from sympy import Symbol, Poly
-from sympy.polys.solvers import RawMatrix
 from sympy.polys.polymatrix import PolyMatrix
 from sympy.matrices.normalforms import invariant_factors, smith_normal_form
 from sympy.polys.domains import ZZ, QQ
 
 def test_smith_normal():
-    m = RawMatrix([[12, 6, 4,8],[3,9,6,12],[2,16,14,28],[20,10,10,20]])
+    m = PolyMatrix([[12, 6, 4,8],[3,9,6,12],[2,16,14,28],[20,10,10,20]])
     setattr(m, 'ring', ZZ)
-    smf = RawMatrix([[1, 0, 0, 0], [0, 10, 0, 0], [0, 0, -30, 0], [0, 0, 0, 0]])
+    smf = PolyMatrix([[1, 0, 0, 0], [0, 10, 0, 0], [0, 0, -30, 0], [0, 0, 0, 0]])
     assert smith_normal_form(m) == smf
 
     x = Symbol('x')
@@ -20,12 +19,12 @@ def test_smith_normal():
     snf, s, t = smith_normal_form(m, full=True)
     assert snf == s*m*t
 
-    m = RawMatrix([[2, 4]])
+    m = PolyMatrix([[2, 4]])
     setattr(m, 'ring', ZZ)
-    smf = RawMatrix([[2, 0]])
+    smf = PolyMatrix([[2, 0]])
     assert smith_normal_form(m) == smf
 
-    m = RawMatrix([[QQ(2), QQ(1)/3], [QQ(4),QQ(2)/3]])
+    m = PolyMatrix([[QQ(2), QQ(1)/3], [QQ(4),QQ(2)/3]])
     setattr(m, "ring", QQ)
     snf, s, t = smith_normal_form(m, full=True)
     assert snf == s*m*t
